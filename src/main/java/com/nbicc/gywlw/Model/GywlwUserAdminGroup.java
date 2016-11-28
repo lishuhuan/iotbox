69c447d21955641974c2f70e566694d9a3470d5d
hadoop <hadoop@master>
2016-11-28 14:44:15 +0800

    lish1444

---
+++ src/main/java/com/nbicc/gywlw/Model/GywlwUserAdminGroup.java
@@ -0,0 +1,58 @@
+package com.nbicc.gywlw.Model;
+
+import java.util.Date;
+
+public class GywlwUserAdminGroup {
+
+	private String id;
+	
+	private String adminId;
+	
+	private String userId;
+	
+	private Date createTime;
+	
+	private int delMark;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public int getDelMark() {
+		return delMark;
+	}
+
+	public void setDelMark(int delMark) {
+		this.delMark = delMark;
+	}
+	
+	
+}
